4adf031d77093ddc289d12413f593a91c24f3fb4
North0n <mautin.ivan.3@gmail.com>
2022-10-14 22:53:18 +0300

    Add interfaces and model

---
+++ DirectoryScanner.Core/Interfaces/IDirectoryScanner.cs
@@ -0,0 +1,12 @@
+﻿using DirectoryScanner.Core.Model;
+
+namespace DirectoryScanner.Core.Interfaces;
+
+public interface IDirectoryScanner
+{
+     FilesystemTree StartScan(string path, ushort maxThreadCount);
+     
+     void StopScan();
+     
+     public bool IsScanning { get; }
+}
---
+++ DirectoryScanner.Core/Model/Node.cs
@@ -0,0 +1,22 @@
+﻿namespace DirectoryScanner.Core.Model;
+
+public class Node
+{
+    public Node(string path, string name)
+    {
+        Path = path;
+        Name = name;
+    }
+
+    public Node(string path, string name, long size)
+    {
+        Path = path;
+        Name = name;
+        Size = size;
+    }
+
+    public string Path { get; }
+    public string Name { get; }
+    public long Size { get; set; }
+    public List<Node>? Children { get; set; } = null;
+}
